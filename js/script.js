7653cc24aa6349a122930cb62509be2dfba6949f
VinRu5 <vincenzorusso.inbox@gmail.com>
2021-06-08 16:41:24 +0200

    revisione finale

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
+//definizione elemaenti in pagina
 var buttonTotal = document.getElementsByClassName('button-total')[0];
 var totalOutput = document.getElementsByClassName('total')[0];
 var textSales = document.getElementsByClassName('add-sale')[0];
-var discount = 20;
 
+//definizione costanti
+var discount = 20;
+var textDiscount = 'SALE20';
+
+//generazione evento click per il calcolo del prezzo finale
 buttonTotal.addEventListener('click', function(){
     var chooseBurger = document.getElementById('choose-burger');
     var checkboxes = document.getElementsByClassName('add-food');
     
+    //scelta del panino 
     for (x = 0; x < chooseBurger.length; x++) {
         if (chooseBurger.value === 'cheese') {
             var burgerPrice = 45;
@@ -19,15 +25,16 @@ buttonTotal.addEventListener('click', function(){
         }
     }
 
+    //somma delle aggiunte
     for (var i = 0; i < checkboxes.length; i++) {
         if (checkboxes[i].checked) {
             burgerPrice += parseInt(checkboxes[i].value);
         }
     }
 
+    //applicazione dello sconto
     var sale = burgerPrice * discount / 100;
-
-    if (textSales.value === 'SALE20') {
+    if (textSales.value === textDiscount) {
         burgerPrice -= sale;
     } else if (textSales.value.length > 0) {
         alert('Codice sconto errato!');
@@ -35,6 +42,7 @@ buttonTotal.addEventListener('click', function(){
 
     burgerPrice = burgerPrice.toFixed(2);
 
+    //stampa del prezzo finale
     totalOutput.innerHTML = burgerPrice;
 });
 
